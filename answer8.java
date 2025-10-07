02f997bde0fb81eaaf766210bdd47cc3d787e849
sumitbarmota <barmotasumit@gmail.com>
2025-10-07 22:04:08 +0530

    Update answer8.java

--- answer8.java
+++ answer8.java
@@ -1,22 +1,25 @@
-#include<iostream>
-using namespace std;
+import java.util.*;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    int arr[105][105];
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-        }
+public class The_Royal_Diagonal {
+    static int primaryDiagonal(int[][] arr, int i) {
+        if (i == arr.length) return 0;
+        return arr[i][i] + primaryDiagonal(arr, i + 1);
     }
-    
-    int diag1 = 0, diag2 = 0;
-    for(int i = 0; i < n; i++) {
-        diag1 += arr[i][i];
-        diag2 += arr[i][n - 1 - i];
+
+    static int secondaryDiagonal(int[][] arr, int i) {
+        if (i == arr.length) return 0;
+        return arr[i][arr.length - i - 1] + secondaryDiagonal(arr, i + 1);
     }
-    
-    cout << diag1 << ", " << diag2 << endl;
-    return 0;
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int sum1 = primaryDiagonal(arr, 0);
+        int sum2 = secondaryDiagonal(arr, 0);
+        System.out.println("Primary Diagonal Sum = " + sum1);
+        System.out.println("Secondary Diagonal Sum = " + sum2);
+    }
 }
